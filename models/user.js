6061a56481fd07bffc6d1a34338589f1bf4c0645
Jason <timeli@naver.com>
2022-06-17 14:59:40 +0900

    기본 세팅 및 모델 정의

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        email: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+          unique: true,
+        },
+        nickname: {
+          type: Sequelize.STRING(30),
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(100),
+          allowNull: true,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: 'User',
+        tableName: 'users',
+        paranoid: true,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+  static associate(db) {
+    db.User.hasMany(db.Movie);
+    db.User.belongsToMany(db.Movie, { through: 'Like', as: 'Liked' });
+    //db.User.belongsToMany(db.Movie, { through: 'Mylist', as: 'List' });
+  }
+};
