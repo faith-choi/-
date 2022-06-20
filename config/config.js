3666f9d10c65054a636ec15fac979501562dc5a8
Sounwoo <mna800@naver.com>
2022-06-20 14:10:02 +0900

    데이터 베이스 수정

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: 'root',
-    password: process.env.SEQUELIZE_PASSWORD,
-    database: 'neckslice',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  test: {
-    username: 'root',
-    password: process.env.SEQUELIZE_PASSWORD,
-    database: 'neckslice',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: process.env.SEQUELIZE_PASSWORD,
-    database: 'neckslice',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
+    development: {
+        username: 'root',
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: 'neckslice',
+        host: 'database-1.ctqwwyyctbbp.ap-northeast-2.rds.amazonaws.com',
+        dialect: 'mysql',
+    },
+    test: {
+        username: 'root',
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: 'neckslice',
+        host: 'database-1.ctqwwyyctbbp.ap-northeast-2.rds.amazonaws.com',
+        dialect: 'mysql',
+    },
+    production: {
+        username: 'root',
+        password: process.env.SEQUELIZE_PASSWORD,
+        database: 'neckslice',
+        host: 'database-1.ctqwwyyctbbp.ap-northeast-2.rds.amazonaws.com',
+        dialect: 'mysql',
+    },
 };
