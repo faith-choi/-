796bb3f290a71164beccd995c2f7028502b83d3b
Sounwoo <mna800@naver.com>
2022-06-20 22:12:02 +0900

    영화

--- router/movie.js
+++ router/movie.js
@@ -3,19 +3,18 @@ const Movie = require('../models/movie.js');
 
 const router = express.Router();
 
-router.post('/', async, (req, res, next) => {
-    const { videoUrl, imgUrl, title, content, category, director, actor } =
-      req.body;
-      await Movie.create({
-      videoUrl,
-      imgUrl,
-      title,
-      content,
-      category,
-      director,
-      actor,
+router.post('/', async (req, res, next) => {
+    const { videoUrl, imgUrl, title, content, category, director, actor } = req.body;
+    await Movie.create({
+        videoUrl,
+        imgUrl,
+        title,
+        content,
+        category,
+        director,
+        actor,
     });
     res.json({ message: '생성완료' });
-  });
+});
 
 module.exports = router;
