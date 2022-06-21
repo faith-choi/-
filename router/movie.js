1e0b7ed59e1efe84c6414f04f964ed8c243f628d
Sounwoo <mna800@naver.com>
2022-06-21 14:39:52 +0900

    메인 비디오 영상

--- router/movie.js
+++ router/movie.js
@@ -21,19 +21,25 @@ const router = express.Router();
 router.get('/movies', async (req, res, next) => {
     try {
         const movie = await Movie.findAll();
-        const movieUrl1 =
-            "https://nexslice.s3.ap-northeast-2.amazonaws.com/'%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C2'+%EB%A9%94%EC%9D%B8+%EC%98%88%EA%B3%A0%ED%8E%B8.mp4";
-        const movieUrl2 =
-            'https://nexslice.s3.ap-northeast-2.amazonaws.com/%EC%98%81%ED%99%94+%5B%EB%A7%88%EB%85%802%5D+%ED%8B%B0%EC%A0%80+%EC%98%88%EA%B3%A0%ED%8E%B8+%EC%8B%A0%EC%8B%9C%EC%95%84%2C+%EB%B0%95%EC%9D%80%EB%B9%88%2C+%EC%84%9C%EC%9D%80%EC%88%98%2C+%EC%9D%B4%EC%A2%85%EC%84%9D%2C+%EA%B9%80%EB%8B%A4%EB%AF%B8+2022.06++%EC%95%A1%EC%85%98.mp4';
+
         res.json({
             movie,
-            movieUrl1,
-            movieUrl2,
         });
     } catch (error) {
         console.log(error);
         next(error);
     }
+});
+
+router.get('/movieUrl', (req, res, next) => {
+    const movieUrl1 =
+        "https://nexslice.s3.ap-northeast-2.amazonaws.com/'%EB%B2%94%EC%A3%84%EB%8F%84%EC%8B%9C2'+%EB%A9%94%EC%9D%B8+%EC%98%88%EA%B3%A0%ED%8E%B8.mp4";
+    const movieUrl2 =
+        'https://nexslice.s3.ap-northeast-2.amazonaws.com/%EC%98%81%ED%99%94+%5B%EB%A7%88%EB%85%802%5D+%ED%8B%B0%EC%A0%80+%EC%98%88%EA%B3%A0%ED%8E%B8+%EC%8B%A0%EC%8B%9C%EC%95%84%2C+%EB%B0%95%EC%9D%80%EB%B9%88%2C+%EC%84%9C%EC%9D%80%EC%88%98%2C+%EC%9D%B4%EC%A2%85%EC%84%9D%2C+%EA%B9%80%EB%8B%A4%EB%AF%B8+2022.06++%EC%95%A1%EC%85%98.mp4';
+    res.json({
+        movieUrl1,
+        movieUrl2,
+    });
 });
 
 router.get('/movie/:movieId', async (req, res, next) => {
