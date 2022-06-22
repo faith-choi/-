f1335e555198db409953241dc28e5dfe2333a2f0
Sounwoo <mna800@naver.com>
2022-06-22 17:36:04 +0900

    로그인 수정

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,17 @@ const User = require('../models/user');
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
-    const [authType, authToken] = (authorization || '').split(' ');
-    console.log(authToken);
-    if (!authToken) {
-        res.status(401).send({
-            errorMessage: '로그인 후 이용 가능한 기능입니다.',
-        });
-        return;
-    }
+    // const [authType, authToken] = (authorization || '').split(' ');
+    // console.log(authToken);
+    // if (!authToken) {
+    //     res.status(401).send({
+    //         errorMessage: '로그인 후 이용 가능한 기능입니다.',
+    //     });
+    //     return;
+    // }
 
     try {
-        const { userId } = jwt.verify(authToken, process.env.JWT_SECRET);
+        const { userId } = jwt.verify(authorization, process.env.JWT_SECRET);
         User.findByPk(userId).then((user) => {
             res.locals.user = user;
             next();
