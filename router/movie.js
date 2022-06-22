53742c333fb2fe0125092dc4cecfed25b44066a8
Sounwoo <mna800@naver.com>
2022-06-22 21:05:41 +0900

    영화 상세페이지

--- router/movie.js
+++ router/movie.js
@@ -21,59 +21,48 @@ const router = express.Router();
 
 // 영화 전체조회
 router.get('/api/movies', Authmiddleware, async (req, res, next) => {
-  // #swagger.tags = ['Movie']
-  try {
-    const user = res.locals.user;
-    const movie = await Movie.findAll({
-      include: [{ model: User, as: 'Likers', attributes: ['id'] }],
-    });
-    //console.log(movie[0]['Likers'][0]['dataValues']['id']);
-    const likers = movie.map((m) =>
-      m['Likers'].map((n) => n['dataValues']['id'])
-    );
-    const likes = likers.map((m) => m.includes(user.id));
+    // #swagger.tags = ['Movie']
+    try {
+        const movie = await Movie.findAll();
 
-    const movies = movie.map((m, i) => {
-      return { movie: m, isLike: likes[i] };
-    });
-    res.json(movies);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+        res.json(movie);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
 });
 
 // 영화 상세조회
 router.get('/api/movie/:movieId', Authmiddleware, async (req, res, next) => {
-  // #swagger.tags = ['Movie']
-  try {
-    const id = req.params.movieId;
-    const user = res.locals.user;
-    const detail = await Movie.findOne({
-      where: { id },
-    });
+    // #swagger.tags = ['Movie']
+    try {
+        const id = req.params.movieId;
+        const user = res.locals.user;
+        const detail = await Movie.findOne({
+            where: { id },
+        });
 
-    const likers = await detail.getLikers();
-    const likersList = likers.map((v) => v.dataValues.id);
-    const lister = await detail.getLister();
-    const listerList = lister.map((v) => v.dataValues.id);
-    let isLike;
-    let isList;
-    if (likersList.includes(user.id)) {
-      isLike = true;
-    } else {
-      isLike = false;
+        const likers = await detail.getLikers();
+        const likersList = likers.map((v) => v.dataValues.id);
+        const lister = await detail.getLister();
+        const listerList = lister.map((v) => v.dataValues.id);
+        let isLike;
+        let isList;
+        if (likersList.includes(user.id)) {
+            isLike = true;
+        } else {
+            isLike = false;
+        }
+        if (listerList.includes(user.id)) {
+            isList = true;
+        } else {
+            isList = false;
+        }
+        res.json({ detail, isLike, isList });
+    } catch (error) {
+        console.log(error);
+        next(error);
     }
-    if (listerList.includes(user.id)) {
-      isList = true;
-    } else {
-      isList = false;
-    }
-    res.json({ detail, isLike, isList });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
 });
 
 module.exports = router;
